407e9059023bf2bccb5e3f7eb25ca633813bd84b
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-08-16 11:20:10 +0430

    Database created and data added

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
-/* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
+  id INTEGER,
+  name VARCHAR(50),
+  date_of_birth DATE,
+  escape_attempts INTEGER,
+  neutered BOOLEAN,
+  weight_kg DECIMAL
 );
