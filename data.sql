198468ac6ea3e75b04eb25184fb1323204f2603d
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-08-17 08:08:50 +0430

    New data added

--- data.sql
+++ data.sql
@@ -10,3 +10,26 @@ VALUES ('pikachu', '2021-01-07', 1, FALSE, 15.04);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('devimon', '2017-05-12', 5, TRUE, 11);
+
+-- update data
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('charmander', '2020-2-8', 0, FALSE, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('plantmon', '2021-11-15', 2, TRUE, -5.7);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('squirtle', '1993-04-02', 3, FALSE, -12.13);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('angemon', '2005-06-12', 1, TRUE, -45);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('boarmon', '2005-06-07', 7, TRUE, 20.4);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('blossom', '1998-10-13', 3, TRUE, 17);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Ditto', '2022-05-14', 4, TRUE, 22);
