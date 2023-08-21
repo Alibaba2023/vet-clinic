5d8df843a32bad5351daa6217145eec74577e36a
Ali Baba <ali7090370@gmail.com>
2023-08-21 21:08:25 +0430

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,67 +1,47 @@
-CREATE TABLE animals (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(50),
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(20),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE animals(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
   date_of_birth DATE,
-  escape_attempts INTEGER,
+  escape_attempts INT,
   neutered BOOLEAN,
-  weight_kg DECIMAL
+  weight_kg DECIMAL,
+  species_id INT REFERENCES species(id),
+  owner_id INT REFERENCES owners(id),
+  PRIMARY KEY(id)
 );
 
--- add column species
-
-ALTER TABLE animals
-ADD COLUMN species VARCHAR(50);
-
--- owners table 
-
-CREATE TABLE owners (
-	id SERIAL PRIMARY KEY,
-	full_name VARCHAR(50),
-	age SMALLINT
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE species (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50)
+CREATE TABLE specializations(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  species_id INT REFERENCES species(id),
+  vet_id INT REFERENCES vets(id),
+  PRIMARY KEY(id)
 );
 
-ALTER TABLE animals
-DROP COLUMN species;
-
-ALTER TABLE animals
-ADD COLUMN species_id INTEGER;
-
-ALTER TABLE animals
-ADD CONSTRAINT fk_animals_species
-FOREIGN KEY (species_id) REFERENCES species(id);
-
-
-ALTER TABLE animals
-ADD COLUMN owner_id INTEGER;
-
-ALTER TABLE animals
-ADD CONSTRAINT fk_animals_owners
-FOREIGN KEY (owner_id) REFERENCES owners(id);
-
-
-CREATE TABLE vets (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50),
-	age SMALLINT,
-	date_of_graduation DATE
-);
-
-
-CREATE TABLE specializations (
-	id BIGSERIAL PRIMARY KEY,
-	vet_id INTEGER REFERENCES vets(id),
-	species_id INTEGER REFERENCES species(id)
-);
-
-
-CREATE TABLE visits (
-	id BIGSERIAL PRIMARY KEY,
-	visit_date DATE,
-	vet_id INTEGER REFERENCES vets(id),
-	animal_id INTEGER REFERENCES animals(id)
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
 );
