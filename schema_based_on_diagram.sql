5deff815b7a6a9696924aa4aa1593cd42b339ca1
Sergio Andres Usma <126805818+sergio-usma@users.noreply.github.com>
2023-08-22 14:14:50 -0500

    Creating index to order tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -31,3 +31,17 @@ CREATE TABLE treatments_medical_histories (
     medical_history_id int REFERENCES medical_histories(id),
     PRIMARY KEY (treatment_id, medical_history_id)
 );
+
+-- medical_histories Table
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories(patient_id);
+
+-- invoices Table
+CREATE INDEX idx_invoices_medical_history_id ON invoices(medical_history_id);
+
+-- invoice_items Table
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items(treatment_id);
+
+-- treatments_medical_histories Table
+CREATE INDEX idx_treatments_medical_histories_treatment_id ON treatments_medical_histories(treatment_id);
+CREATE INDEX idx_treatments_medical_histories_medical_history_id ON treatments_medical_histories(medical_history_id);
